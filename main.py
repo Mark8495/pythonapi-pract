4b6c81c12bfc167527f008421982fac10cda7d6d
Mark8495 <96879727+Mark8495@users.noreply.github.com>
2023-05-04 15:08:06 +0000

    created payload

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def get_posts():
     return{"Data" : "This is your posts"}
 
 @app.get("/createposts")
-def create_posts(payLoad: dict = Body):
-    return{"message" : "Created Post"}
+def create_posts(payLoad: dict = Body(...)):
+    return{"new_post" : f"title {payLoad['title']} conten: {payLoad['content']}"}
 
 
 
